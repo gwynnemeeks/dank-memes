0070c27c965b3ce46e8b2f4e7bf58d7173d5dfc1
Gwynne Meeks <meeks.gwynne@gmail.com>
2021-03-26 10:48:19 -0500

    adding links with styling complete

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,13 @@
     <v-app-bar app color="pink lighten-2" light>
       <h1>Vue Meme Forum</h1>
       <v-spacer></v-spacer>
-      <router-link to="/create"> Create </router-link>
+      <router-link to="/create">
+        <v-btn text>Create</v-btn>
+      </router-link>
       |
-      <router-link to="/feed"> Memes </router-link>
+      <router-link to="/feed">
+        <v-btn text> Memes </v-btn>
+      </router-link>
     </v-app-bar>
 
     <v-main>
@@ -25,7 +29,7 @@ a {
 h1 {
   color: rgb(83, 35, 49);
 }
-.router-link-active {
+.router-link-active .v-btn {
   color: rgb(83, 35, 49);
 }
 </style>
