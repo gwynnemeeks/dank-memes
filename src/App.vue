2a8aa3363f04ac80047c909fbd74c44e9fa7ede8
Gwynne Meeks <meeks.gwynne@gmail.com>
2021-03-26 10:41:07 -0500

    wip links

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <v-app>
-    <v-app-bar app color="pink lighten-2" light> ??? </v-app-bar>
+    <v-app-bar app color="pink lighten-2" light>
+      <h1>Vue Meme Forum</h1>
+      <v-spacer></v-spacer>
+      <router-link to="/create"> Create </router-link>
+      |
+      <router-link to="/feed"> Memes </router-link>
+    </v-app-bar>
 
     <v-main>
       <router-view></router-view>
