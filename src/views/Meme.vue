b6f7d22b7bba7b2226f8f8f9c882d339ae47d7d4
Gwynne Meeks <meeks.gwynne@gmail.com>
2021-03-29 10:36:49 -0500

    wip add meme component

---
+++ src/views/Meme.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-img :src="imageURL" :width="800">
+    <h1 id="top">{{ top.toUpperCase() }}</h1>
+    <h1 id="bottom">{{ bottom.toUpperCase() }}</h1>
+  </v-img>
+</template>
+
+<script>
+export default {
+  props: ["top", "bottom", "imageURL"],
+};
+</script>
+
+<style></style>
