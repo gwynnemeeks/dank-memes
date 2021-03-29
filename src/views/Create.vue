2f5d79e5517f87efaa32bb345fb06923917a74ab
Gwynne Meeks <meeks.gwynne@gmail.com>
2021-03-29 11:28:39 -0500

    form and meme styling complete

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -1,15 +1,49 @@
 <template>
-  <div>
-    <form @submit.prevent="generateMeme">
-      <input placeholder="Image URL" type="text" v-model="imageURL" />
-      <input placeholder="Top Text" type="text" v-model="topText" />
-      <input placeholder="Bottom Text" type="text" v-model="bottomText" />
-      <button type="submit">Generate Meme!</button>
-    </form>
+  <v-container>
+    <v-form @submit.prevent="generateMeme">
+      <v-row justify="space-between">
+        <v-col>
+          <v-text-field
+            outlined
+            v-model="imageURL"
+            type="text"
+            label="Image URL"
+          />
+        </v-col>
+        <v-col>
+          <v-text-field
+            outlined
+            v-model="topText"
+            type="text"
+            label="Top Text"
+          />
+        </v-col>
+        <v-col>
+          <v-text-field
+            outlined
+            v-model="bottomText"
+            type="text"
+            label="Bottom Text"
+          />
+        </v-col>
+      </v-row>
+      <v-row v-if="!showMeme" justify="center">
+        <v-btn :disabled="!imageURL" text color="primary" type="submit">
+          Generate meme!
+        </v-btn>
+      </v-row>
+    </v-form>
+
     <div v-if="showMeme">
-      <meme :top="topText" :bottom="bottomText" :imageURL="imageURL" />
+      <meme
+        class="mx-auto"
+        :top="topText"
+        :bottom="bottomText"
+        :imageURL="imageURL"
+        :width="800"
+      />
     </div>
-  </div>
+  </v-container>
 </template>
 
 <script>
